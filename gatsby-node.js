597bb686ce0dbfad72dfd6225207b34f276e7af1
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2020-02-04 22:01:10 +0100

    Remove sccents

--- gatsby-node.js
+++ gatsby-node.js
@@ -88,6 +88,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
+    console.log(node)
     const value = createFilePath({
       node,
       getNode,
